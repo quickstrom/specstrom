5eee4f5ebf777e26247656e1f3cdb64846334243
Oskar Wickström <oskar@wickstrom.tech>
2021-02-05 18:12:20 +0100

    Print usage and errors on stderr

--- Main.hs
+++ Main.hs
@@ -1,19 +1,23 @@
+{-# LANGUAGE LambdaCase #-}
 module Main where
-import Lexer
-import Parser 
+
 import Evaluator
+import Lexer
+import Parser
 import PrettyPrinter
 import System.Environment
 import qualified Text.PrettyPrint.ANSI.Leijen as P
+import System.IO (hPutStrLn, stderr, hPutStr)
+
 run :: String -> String -> IO ()
-run f s = do 
-  case lexer (f,1,1) s of 
-    Left e -> P.putDoc (prettyLexerError e) 
-    Right ts -> case parseBindingBody builtIns ts of 
-      Left e -> P.putDoc (prettyParseError e)
-      Right (_,b) -> print $ evaluate initialEnv b 
+run f s = do
+  case lexer (f, 1, 1) s of
+    Left e -> P.hPutDoc stderr (prettyLexerError e)
+    Right ts -> case parseBindingBody builtIns ts of
+      Left e -> P.hPutDoc stderr (prettyParseError e)
+      Right (_, b) -> print $ evaluate initialEnv b
   putStrLn ""
-  
 
-
-main = getArgs >>= \[f] -> readFile f >>= run f
+main = getArgs >>= \case
+  [f] -> readFile f >>= run f
+  _ -> hPutStrLn stderr "Usage: specstrom FILE"
