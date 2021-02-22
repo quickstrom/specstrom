319763c0b0d7e045371c7178813a8517d511ff6a
owickstrom <owickstrom@users.noreply.github.com>
2021-02-22 19:38:51 +0000

    Auto-format source code

--- test/Specstrom/StreamingVerifierTest.hs
+++ test/Specstrom/StreamingVerifierTest.hs
@@ -5,9 +5,9 @@ module Specstrom.StreamingVerifierTest where
 
 import Hedgehog (Gen, Property, annotateShow, checkParallel, discover, failure, forAll, property, (===))
 import qualified Hedgehog.Gen as Gen
-import Specstrom.StreamingVerifier
 import qualified Hedgehog.Range as Range
 import qualified Pipes
+import Specstrom.StreamingVerifier
 
 prop_always_eventually_equivalence_list :: Property
 prop_always_eventually_equivalence_list = property $ do
@@ -17,9 +17,9 @@ prop_always_eventually_equivalence_list = property $ do
   let p = is state
   case (verifyList (always n p) trace, verifyList (formulaNegate (eventually n (formulaNegate p))) trace) of
     (Right r1, Right r2) -> r1 === r2
-    (Left CannotStep{}, Left CannotStep{}) -> pure ()
-    (Left UnexpectedEndFormula{}, _) -> failure
-    (_, Left UnexpectedEndFormula{}) -> failure
+    (Left CannotStep {}, Left CannotStep {}) -> pure ()
+    (Left UnexpectedEndFormula {}, _) -> failure
+    (_, Left UnexpectedEndFormula {}) -> failure
     (r1, r2) -> annotateShow (r1, r2) >> failure
 
 prop_always_eventually_equivalence_stream :: Property
@@ -30,13 +30,13 @@ prop_always_eventually_equivalence_stream = property $ do
   let p = is state
   case (verify (always n p) (Pipes.each trace), verify (formulaNegate (eventually n (formulaNegate p))) (Pipes.each trace)) of
     (Right r1, Right r2) -> r1 === r2
-    (Left CannotStep{}, Left CannotStep{}) -> pure ()
-    (Left UnexpectedEndFormula{}, _) -> failure
-    (_, Left UnexpectedEndFormula{}) -> failure
+    (Left CannotStep {}, Left CannotStep {}) -> pure ()
+    (Left UnexpectedEndFormula {}, _) -> failure
+    (_, Left UnexpectedEndFormula {}) -> failure
     (r1, r2) -> annotateShow (r1, r2) >> failure
 
 genTrace :: Gen Trace
-genTrace = Gen.string (Range.linear 1 20) genState 
+genTrace = Gen.string (Range.linear 1 20) genState
 
 genState :: Gen State
 genState = Gen.element "abc"
