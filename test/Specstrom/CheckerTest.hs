7e25cff06469384e7c32bdf8d216936a815ffdbb
Oskar Wickström <oskar.wickstrom@ingka.com>
2021-03-27 09:30:47 +0100

    WIP

--- test/Specstrom/CheckerTest.hs
+++ test/Specstrom/CheckerTest.hs
@@ -12,6 +12,8 @@ import Data.Text (Text)
 import qualified Data.Text.Prettyprint.Doc as Doc
 import Data.Text.Prettyprint.Doc.Render.String
 import Hedgehog (Property, annotateShow, assert, checkParallel, discover, evalIO, forAll, property)
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
 import qualified Specstrom.Analysis as Analysis
 import qualified Specstrom.Channel as Channel
 import qualified Specstrom.Checker as Checker
@@ -27,23 +29,24 @@ prop_check_produces_result = property $ do
   ts <- evalIO (load "test/Specstrom/CheckerTest/next")
   let dep = foldMap Analysis.depOf (Analysis.analyseTopLevels ts)
   annotateShow dep
-  state <- forAll (Gen.state dep)
+  states <- forAll (Gen.list (Range.linear 2 10) (Gen.state dep))
   (interpreterRecv, interpreterSend) <- Channel.newChannel
   (executorRecv, executorSend) <- Channel.newChannel
   let send = Channel.send executorSend
   results <- evalIO $
     Async.withAsync (Checker.checkAll executorRecv interpreterSend ts) $ \checker -> do
-      let runSessions = do
+      let runSessions [] = fail "Not enough states"
+          runSessions (s:ss) = do
             Channel.receive interpreterRecv >>= \case
               Protocol.RequestAction {} -> do
-                Channel.send executorSend (Protocol.Performed state)
-                runSessions
+                Channel.send executorSend (Protocol.Performed s)
+                runSessions ss
               Protocol.Start {} -> do
-                send (Protocol.Event Evaluator.Loaded state)
-                runSessions
-              Protocol.End {} -> runSessions
+                send (Protocol.Event Evaluator.Loaded s)
+                runSessions ss
+              Protocol.End {} -> runSessions ss
               Protocol.Done {} -> pure ()
-      runSessions
+      runSessions states
       Async.waitCatch checker
   annotateShow results
   assert (isRight results)
