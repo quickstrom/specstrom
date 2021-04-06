5e0eacbfe9f19acd1b3ed0ec2392f7893cca63b4
owickstrom <oskar@wickstrom.tech>
2021-04-06 08:11:35 +0200

    Add flag for enabling profiling build

--- default.nix
+++ default.nix
@@ -1,8 +1,12 @@
-{ pkgs ? import <nixpkgs> {}, compiler ? "ghc884" }:
+{ pkgs ? import <nixpkgs> {}, compiler ? "ghc884", enableProfiling ? false }:
 let
   src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
   haskellPackages = pkgs.haskell.packages.${compiler};
+  drv = (haskellPackages.callCabal2nix "specstrom" "${src}" { });
 in {
   inherit haskellPackages;
-  specstrom = pkgs.haskell.lib.justStaticExecutables (haskellPackages.callCabal2nix "specstrom" "${src}" { });
+  specstrom = 
+    if enableProfiling 
+    then pkgs.haskell.lib.enableExecutableProfiling drv
+    else pkgs.haskell.lib.justStaticExecutables drv;
 }
