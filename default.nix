64fff6d04c1793f7c84564a3e6e658caed0f8283
owickstrom <oskar@wickstrom.tech>
2021-04-11 19:26:37 +0200

    Make haskeline work with nix

--- default.nix
+++ default.nix
@@ -1,7 +1,11 @@
 { pkgs ? import <nixpkgs> {}, compiler ? "ghc884", enableProfiling ? false }:
 let
   src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
-  haskellPackages = pkgs.haskell.packages.${compiler};
+  haskellPackages = pkgs.haskell.packages.${compiler}.override {
+    overrides = self: super: {
+      haskeline = pkgs.haskell.lib.dontCheck (self.callHackage "haskeline" "0.8.0.0" {});
+    };
+  };
   drv = (haskellPackages.callCabal2nix "specstrom" "${src}" { });
 in {
   inherit haskellPackages;
